40e36b9d98105fbddf693c68e6f567b4c1edcfdc
khaliloualdchaib <oualdchaibkhalil@gmail.com>
2025-02-15 22:12:30 +0100

    style predictor

--- FashionDataset.py
+++ FashionDataset.py
@@ -43,5 +43,5 @@ transform = transforms.Compose([
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomRotation(degrees=10),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std= [0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.81689762, 0.8230991, 0.84754206], std=[0.28189409, 0.278586, 0.25931382]),
 ])
